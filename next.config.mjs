c8dfcf345f9df231d3c1b5a332ddc636086a0468
Saumay Paul <saumay@chainflux.com>
2024-05-21 23:54:59 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 // const isProd = process.env.NODE_ENV === 'github-pages'
 const nextConfig = {
-    reactStrictMode: true,
     basePath: "/saumaypaul",
+    output: "export",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
