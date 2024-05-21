77b743474a9d36deb3f3fca16c33d8fcba5967d2
Saumay Paul <saumay@chainflux.com>
2024-05-21 23:27:51 +0530

    basepath next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output:isProd ? "export": undefined,
     reactStrictMode: true,
-    assetPrefix: isProd ? 'https://saumaypaul20.github.io/saumaypaul/' : undefined,
+    basePath: isProd ? '/saumaypaul' : undefined,
 };
 
 export default nextConfig;
