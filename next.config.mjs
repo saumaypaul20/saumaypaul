32e48b64b322a4aacf28d62db909ba7aba5b39ac
Saumay Paul <saumaypaul20@gmail.com>
2024-05-22 03:46:55 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'github-pages'
 const nextConfig = {
     basePath: "/saumaypaul",
     output: "export",
-    reactStrictMode: true,
-    assetPrefix: isProd ?"/saumaypaul": undefined
+    reactStrictMode: true
 };
 
 export default nextConfig;
