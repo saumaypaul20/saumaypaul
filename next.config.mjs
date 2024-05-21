8177307030de851e3877622a32356e2585074cb3
Saumay Paul <saumay@chainflux.com>
2024-05-21 23:35:39 +0530

    github-pages nextconfig added

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'github-pages'
 const nextConfig = {
     output:isProd ? "export": undefined,
     reactStrictMode: true,
